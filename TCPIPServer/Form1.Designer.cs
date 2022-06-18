433c1a90c89be57e64dcf55d3a39850b5a6542d9
José Pazmiño <pepe.paz0399@gmail.com>
2022-06-18 11:13:32 -0500

    Make the app and create published folder

---
+++ TCPIPServer/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace TCPIPServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(265, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Port:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Host:";
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(107, 49);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(125, 27);
+            this.txtHost.TabIndex = 3;
+            this.txtHost.Text = "127.0.0.1";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(325, 49);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(125, 27);
+            this.txtPort.TabIndex = 4;
+            this.txtPort.Text = "2000";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(490, 49);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(94, 29);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "Iniciar";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(612, 49);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(94, 29);
+            this.btnStop.TabIndex = 6;
+            this.btnStop.Text = "Detener";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(41, 168);
+            this.txtStatus.Multiline = true;
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(665, 325);
+            this.txtStatus.TabIndex = 7;
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(41, 110);
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(543, 27);
+            this.txtSend.TabIndex = 8;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(612, 110);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(94, 29);
+            this.btnSend.TabIndex = 9;
+            this.btnSend.Text = "Enviar";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Establecer Servidor:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(755, 551);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtHost);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "TCP/IP SERVER";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ TCPIPServer/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//********* namespaces Necesarios
+using WatsonTcp;
+using System.Net;
+namespace TCPIPServer
+{
+    public partial class Form1 : Form
+    {
+        WatsonTcpServer server = null; // Declarar el servidor
+        Dictionary<string, string> clientes = null; // Declarar el Diccionario para Clientes
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (server != null) return; // Se retorna si ya existe un servidor
+            // Activar/Desactivar Botones 
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+            btnSend.Enabled = true;
+
+            clientes = new Dictionary<string, string>(); // Crear nueva instancia para el diccionario de clientes
+
+            txtStatus.Text += "Servidor Iniciado..." + "\r\n";
+            string ip = IPAddress.Parse(txtHost.Text).ToString(); // obtner la ip para el host
+            int port = Convert.ToInt32(txtPort.Text); // obtener el puerto para el host
+
+            server = new WatsonTcpServer(ip, port); // Crear nueva instancia del servidor
+            // Eventos que maeja WatsonTcp
+            server.Events.ClientConnected += ClientConnected; // Se dispara al conectarse un cliente
+            server.Events.ClientDisconnected += ClientDisconnected; // Se dispara al desconectarse un cliente
+            server.Events.MessageReceived += MessageReceived; // Se dispara al Recibir un Mensaje de un cliente
+            server.Callbacks.SyncRequestReceived = SyncRequestReceived; // Se dispara al recibir una petición Sincronica
+            server.Start();
+        }
+
+        private void ClientConnected(object sender, ConnectionEventArgs args)
+        {
+            string ipClient = args.IpPort.Substring(0, args.IpPort.IndexOf(":"));
+            if (!clientes.ContainsKey(ipClient))
+            {
+                clientes.Add(ipClient, args.IpPort);
+            }
+            string text = "Cliente Conectado: " + args.IpPort + "\r\n";
+            changeStatus(text);
+        }
+
+        static void ClientDisconnected(object sender, DisconnectionEventArgs args)
+        {
+           // Console.WriteLine("Client disconnected: " + args.IpPort + ": " + args.Reason.ToString());
+        }
+
+        private void MessageReceived(object sender, MessageReceivedEventArgs args)
+        {
+            
+            if (args.Metadata == null)
+            {
+                server.Send(args.IpPort, "Conectado al Servidor");
+            }
+            else
+            {
+                Dictionary<object, object> md = args.Metadata;
+                string action = (string)md["action"];
+                string type = (string)md["type"];
+                switch (action)
+                {
+                    case "sendTxt":
+                        sendText(type, md, args);
+                        break;
+                    case "sendImg":
+                        sendImg(type, md, args);
+                        break;
+                }
+            }
+          
+        }
+        private void Broadcast(string msg, Dictionary<object, object> md)
+        {
+
+            if (server.ListClients().Count() > 0)
+            {
+                foreach (string ip in server.ListClients())
+                {
+                    string ips = ip;
+                    server.Send(ip, msg, md);
+                }
+            }
+            else
+            {
+                txtStatus.Text += "No Existen Clientes Conectados \r\n";
+            }
+        }
+        private void Broadcast(byte[] data, Dictionary<object, object> md)
+        {
+
+            if (server.ListClients().Count() > 0)
+            {
+                foreach (string ip in server.ListClients())
+                {
+                    server.Send(ip, data, md);
+                }
+            }
+            else
+            {
+                txtStatus.Text += "No Existen Clientes Conectados \r\n";
+            }
+        }
+        private void sendText(string type, Dictionary<object, object> md, MessageReceivedEventArgs args)
+        {
+            Dictionary<object, object> mdSend = new Dictionary<object, object>();
+            mdSend.Add("type", "text");
+            if (type.Equals("one"))
+            {
+
+                string ipClient = clientes[(string)md["ipClient"]];
+                string msg = Encoding.UTF8.GetString(args.Data);
+                server.Send(ipClient, msg, mdSend);
+                string text = "El cliente: " + args.IpPort + ": Envio un mensaje al cliente: " + ipClient + " con el contenido: " + Encoding.UTF8.GetString(args.Data) + "\r\n";
+                changeStatus(text);
+            }
+            else if (type.Equals("broadcast"))
+            {
+                string text = "El cliente: " + args.IpPort + ": Envio un mensaje a todos los Clientes con el contenido: " + Encoding.UTF8.GetString(args.Data) + "\r\n";
+                changeStatus(text);
+                string msg = Encoding.UTF8.GetString(args.Data);
+                Broadcast(msg, mdSend);
+            }
+        }
+        private void sendImg(string type, Dictionary<object, object> md, MessageReceivedEventArgs args)
+        {
+            Dictionary<object, object> mdSend = new Dictionary<object, object>();
+            mdSend.Add("type", "img");
+            if (type.Equals("one"))
+            {
+                String ipClient = clientes[(String)md["ipClient"]];
+                byte[] data = args.Data;
+                server.Send(ipClient, data, mdSend);
+                string text = "El cliente: " + args.IpPort + ": Envio una Imagen al Cliente: " + ipClient + "\r\n";
+                changeStatus(text);
+
+            }
+            else if (type.Equals("broadcast"))
+            {
+                byte[] data = args.Data;
+                Broadcast(data, mdSend);
+                string text = "El cliente: " + args.IpPort + ": Envio una Imagen a Todos los Clientes \r\n";
+                changeStatus(text);
+            }
+        }
+
+            static SyncResponse SyncRequestReceived(SyncRequest req)
+        {
+            return new SyncResponse(req, "Conectado");
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (server != null)
+            {
+                server.Stop();
+                server = null;
+                txtStatus.Clear();
+            }
+
+            txtStatus.Text = "Servidor Detenido";
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+            btnSend.Enabled = false;
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            Dictionary<object, object> mdSend = new Dictionary<object, object>();
+            mdSend.Add("type", "text");
+            string msg = txtSend.Text;
+            this.Broadcast(msg,mdSend);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnStop.Enabled = false;
+            btnSend.Enabled = false;
+        }
+        private void changeStatus(string text)
+        {
+            txtStatus.Invoke((MethodInvoker)delegate ()
+            {
+                txtStatus.Text += text;
+
+            });
+        }
+    }
+}
